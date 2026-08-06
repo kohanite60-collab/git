3ad26d4acff2f95243e91a0a97b5f5811930ceb1
cyr <2635521871@qq.com>
2026-08-06 17:16:33 +0800

    第一次jwt功能添加

--- fzu-treehole/src/main/java/org/example/gpt/entity/user.java
+++ fzu-treehole/src/main/java/org/example/gpt/entity/user.java
@@ -1,10 +1,13 @@
 package org.example.gpt.entity;
 
 public class user {
+    private int UserId;
     private String name;
     private String password;
 
     // 必须有 getter / setter
+    public int getUserId() { return UserId; }
+    public void setUserId(int UserId) { this.UserId = UserId; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
---
+++ fzu-treehole/src/main/java/org/example/gpt/utils/JwtUtils.java
@@ -0,0 +1,88 @@
+package org.example.gpt.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final String secret ="113252535353535353666667777777777489958904049487857849475784829292929292";
+    private final long expire = 1000*60*60*24;
+
+    private final SecretKey key= Keys.hmacShaKeyFor(secret.getBytes());
+
+
+
+
+    public String createjwt(int userid){
+        // 创建JWT构建器实例
+
+         String token = Jwts.builder()
+                .setSubject("userid")
+                // 创建时间
+                .setIssuedAt(
+                        new Date()
+                )
+
+                // 过期时间
+                .setExpiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + 1000 * 60 * 60 * 2
+                        )
+                )
+
+
+                // 签名
+                .signWith(
+                        key,
+                        SignatureAlgorithm.HS256
+                )
+
+                // 生成字符串
+                .compact();
+
+        return token;
+
+    }
+
+    /**
+     * 解析JWT
+     */
+    public Long parseToken(String token){
+
+
+        Claims claims =
+                Jwts.parserBuilder()
+
+                        // 使用同一个密钥验证
+                        .setSigningKey(key)
+
+                        .build()
+
+                        //解析token
+                        .parseClaimsJws(token)
+
+                        //获取Payload
+                        .getBody();
+
+
+
+        return Long.valueOf(
+                claims.getSubject()
+        );
+
+    }
+
+
+
+
+
+    }
